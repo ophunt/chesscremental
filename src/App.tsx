e396a1e40d6e764f25fa4745392ed4fd874af6ad
Owen Hunt <ophunt2000@gmail.com>
2021-09-19 21:07:45 -0500

    Manage chess state

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import MoveOdds from "./MoveOdds";
 import "./App.scss";
 import Chessboard from "./Chessboard";
+import { Chess } from "@lubert/chess.ts";
 
 export default function App() {
+  const [chess, setChess] = useState(new Chess());
+  const [fen, setFen] = useState(chess.fen());
+
+  const reset = () => {
+    chess.reset();
+    setFen(chess.fen());
+  };
+
+  const removePieceOnSquare = (sq: string) => {
+    chess.remove(sq);
+    setFen(chess.fen());
+  };
+
   return (
     <div className="App">
       <div className="AppMain">
         <p>Upgrades here</p>
-        <Chessboard />
+        <Chessboard fen={fen} />
         <MoveOdds />
       </div>
     </div>
--- src/Chessboard.tsx
+++ src/Chessboard.tsx
@@ -1,28 +1,12 @@
-import React, { useState } from "react";
-import { Chess } from "@lubert/chess.ts";
+import React from "react";
 import Chessground from "react-chessground";
 import "react-chessground/dist/styles/chessground.css";
 import "./Chessboard.scss";
 
-export default function Chessboard() {
-  const [chess, setChess] = useState(new Chess());
-  const [fen, setFen] = useState(chess.fen());
-
-  const reset = () => {
-    chess.reset();
-    setFen(chess.fen());
-  };
-
-  const removePieceOnSquare = (sq: string) => {
-    chess.remove(sq);
-    setFen(chess.fen());
-  };
-
+export default function Chessboard({ fen }: { fen: string }) {
   return (
     <div className="Chessboard">
       <Chessground fen={fen} />
-      <button onClick={() => removePieceOnSquare("e1")}>Remove King</button>
-      <button onClick={reset}>Reset</button>
     </div>
   );
 }
